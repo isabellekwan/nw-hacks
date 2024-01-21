a97eab75a03eebcafa1181ab71d577fd002e53c8
Ethan Saw <ethansaw@gmail.com>
2024-01-20 21:40:10 -0800

    Added templates for db querying

--- back/hello.py
+++ back/hello.py
@@ -7,3 +7,26 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/users", methods = ['POST'])
+def create_user():
+    # post the new user to db
+    return True
+
+@app.route("/users<string:id>", methods = ['GET'])
+def get_user_by_id():
+    # return user info if exists
+    return True
+
+@app.route("/users<string:id>", methods = ['PUT'])
+def update_user():
+    # update the user in db
+    return True
+
+@app.route("/users<string:id>", methods = ['DELETE'])
+def delete_user():
+    #delete user from db
+    return True
+
+if __name__ == '__main__':
+   app.run(port=5000)
