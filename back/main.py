936e9942a43483892b920760b67b108f4225a87b
Evan <endyce@gmail.com>
2024-01-20 21:36:35 -0800

    Returning users but in bad format

--- back/main.py
+++ back/main.py
@@ -1,14 +1,34 @@
 # hello.py
 
 from flask import Flask
+import psycopg2 as psy
+from os import environ
 
 import dbinit # kinda weird but this just runs the dbinit
 
 app = Flask(__name__)
 
+def create_db_conn():
+    connection = psy.connect(
+        host="db", # service name in docker compse
+        database="postgres",
+        user=environ['DB_USERNAME'],
+        password=environ['DB_PASSWORD'])
+    
+    return connection
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    connection = create_db_conn()
+    
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    users = cursor.fetchall()
+    
+    cursor.close()
+    connection.close()
+    
+    return users
 
 
 if __name__ == "__main__":
