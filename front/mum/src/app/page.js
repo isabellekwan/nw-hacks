5e089a4b0e90bff39536abc63c64a256ebc80772
Eunsong Koh <eka74@sfu.ca>
2024-01-21 01:32:14 -0800

    added folder structure

--- front/mum/src/app/page.js
+++ front/mum/src/app/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Login from "@/pages/login";
-import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Login />
-      <Button>Cheese</Button>
     </main>
   );
 }
